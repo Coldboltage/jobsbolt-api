4ad6e72e97411649476a93a941fe84903d2078a0
Coldboltage <alandreid@hotmail.co.uk>
2024-08-03 12:01:11 +0100

    🚧 added LF and barebones entities

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { JobTypeModule } from './job-type/job-type.module';
+import { JobModule } from './job/job.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, JobTypeModule, JobModule],
   controllers: [AppController],
   providers: [AppService],
 })
